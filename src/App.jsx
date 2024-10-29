93bfed31d623b9b08c9f50d8439756287c8407c2
seyhongkin <kin19seyhong@gmail.com>
2024-10-29 23:45:47 +0700

    init_commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Navbar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Price from "./components/Price";
+import Testimonial from "./components/Testimonial";
+import Footer from "./components/Footer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Price />
+        <Testimonial />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
