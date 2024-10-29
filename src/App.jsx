38accde637ac46d98843f3bdef15c24dc8a1865b
seyhongkin <kin19seyhong@gmail.com>
2024-10-29 23:54:22 +0700

    fix_deploy_fail

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FeatureSection from "./components/FeatureSection";
 import Workflow from "./components/Workflow";
